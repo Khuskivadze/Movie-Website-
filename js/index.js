596a229c1e170e4cbdde1cb7191ccac9c251b439
Tamar  khuskivadze <86792404+Khuskivadze@users.noreply.github.com>
2022-06-26 20:30:13 +0400

    fulll

---
+++ js/index.js
@@ -0,0 +1,54 @@
+ var swiper = new Swiper(".mySwiper",{
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    autoplay:{
+    delay:5000,
+    disableOnInteraction: false,
+
+  },
+  navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+ },  
+ breakpoints:{    
+     640: {
+            slidesPerView : 2,
+            spaceBetween: 20,
+        },
+     768: {
+            slidesPerView : 4,
+            spaceBetween: 40,
+        },
+     1024:{
+            slidesPerView : 3,
+            spaceBetween: 50,
+        
+        },
+    },
+
+});
+
+
+$(window).scroll(function(){
+    if($(document).scrollTop()>20){
+        $('.menu-icon').addClass('fix-icon');
+    }
+    else {
+        $('.menu-icon').removeClass('fix-icon');
+    }
+});
+
+let scrollPrecentrage = () =>{""
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round(pos * 100/ calcHeight);
+    scrollProgress.style.background = 'conic-gradiet(#e70634 ${scrollValue}%, #2b2f38 ${scrollValue}%)';
+}
+window.onscroll = scrollPrecentrage;
+window.onload = scrollPrecentrage;
